0ee256ebae77252e4b2696e5b4148c59bd2907de
lucielong <lucie.long@epfedu.fr>
2023-10-10 15:44:27 +0200

    Model of comment

--- src/main/java/com/example/backbyteacookie/models/Comment.java
+++ src/main/java/com/example/backbyteacookie/models/Comment.java
@@ -1,4 +1,26 @@
 package com.example.backbyteacookie.models;
 
-public record Comment() {
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.time.ZonedDateTime;
+
+@Entity
+@Table(name = "comments")
+@Builder
+
+public record Comment(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        Long id,
+
+        int author_id,
+        int recipe_id,
+        int rating,
+        String body,
+        ZonedDateTime date)
+
+    {
+
 }
