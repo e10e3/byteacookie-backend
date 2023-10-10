3b3d8d9bb1567776ca414ba1f890b34ab9ec2787
Émile <emile.royer@epfedu.fr>
2023-10-10 15:23:10 +0200

    Fill in the User model

--- src/main/java/com/example/backbyteacookie/models/User.java
+++ src/main/java/com/example/backbyteacookie/models/User.java
@@ -1,4 +1,21 @@
 package com.example.backbyteacookie.models;
 
-public record User() {
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor(force = true)
+public record User(
+        @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+        Long id,
+        String name,
+        String email,
+        String password,
+        @OneToMany(mappedBy = "author_id")
+        List<Recipe> recipes,
+        @OneToMany(mappedBy = "author_id")
+        List<Comment> comments) {
 }
