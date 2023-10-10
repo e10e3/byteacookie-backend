396725dbd89b02b1037f1c3d7cc514489b689c0c
Émile <emile.royer@epfedu.fr>
2023-10-10 15:15:19 +0200

    Change the schema to use 64 bits ids

--- initdb/1_TABLES.sql
+++ initdb/1_TABLES.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (
-    id serial PRIMARY KEY,
+    id bigserial PRIMARY KEY,
     name text NOT NULL,
     email text NOT NULL,
     password text NOT NULL
@@ -8,7 +8,7 @@ CREATE TABLE users (
 CREATE TYPE difficulty AS ENUM ( 'easy', 'medium', 'hard' );
 
 CREATE TABLE recipes (
-    id serial PRIMARY KEY,
+    id bigserial PRIMARY KEY,
     name text NOT NULL,
     image bytea,
     description text NOT NULL,
@@ -21,21 +21,21 @@ CREATE TABLE recipes (
 CREATE TYPE ingredient_type AS ENUM ( 'discreet', 'mass', 'volume' );
 
 CREATE TABLE ingredients (
-    id serial PRIMARY KEY,
+    id bigserial PRIMARY KEY,
     name text NOT NULL,
     image bytea,
     type ingredient_type NOT NULL
 );
 
 CREATE TABLE ingredient_recipe (
-    id serial PRIMARY KEY,
+    id bigserial PRIMARY KEY,
     recipe_id integer REFERENCES recipes ON DELETE CASCADE,
     ingredient_id integer REFERENCES ingredients ON DELETE RESTRICT ,
     quantity real NOT NULL
 );
 
 CREATE TABLE comments (
-    id serial PRIMARY KEY,
+    id bigserial PRIMARY KEY,
     author_id integer REFERENCES users ON DELETE CASCADE,
     recipe_id integer REFERENCES recipes ON DELETE CASCADE,
     rating integer CHECK (rating >= 0 and rating <= 10) NOT NULL,
