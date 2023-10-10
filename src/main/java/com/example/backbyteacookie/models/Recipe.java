c1f167f6574d60b642a0e65cccc0fb06c91958eb
Julie Dlg <doligez.julie@gmail.com>
2023-10-10 15:45:29 +0200

    Recipe model

--- src/main/java/com/example/backbyteacookie/models/Recipe.java
+++ src/main/java/com/example/backbyteacookie/models/Recipe.java
@@ -1,4 +1,34 @@
 package com.example.backbyteacookie.models;
 
-public record Recipe() {
+import jakarta.persistence.*;
+import lombok.Builder;
+
+
+import java.time.ZonedDateTime;
+import java.time.*;
+import java.util.List;
+
+@Entity
+@Table(name = "recipe")
+@Builder
+public record Recipe(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        Long id,
+        String name,
+        byte[] image,
+        String description,
+        Difficulty difficulty,
+        Duration time,
+        Long author_id,
+        ZonedDateTime date,
+
+        @ManyToMany
+        @JoinTable(
+        name = "ingredient_recipe",
+        joinColumns = @JoinColumn(name = "ingredient_id"),
+        inverseJoinColumns = @JoinColumn(name = "recipe_id"))
+        List<Ingredient> ingredients
+        ) {
+
 }
