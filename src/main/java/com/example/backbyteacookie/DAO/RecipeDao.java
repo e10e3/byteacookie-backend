38c0f4f6484d07ce12b1699eb6bda714a48d939a
Julie Dlg <doligez.julie@gmail.com>
2023-10-10 16:10:59 +0200

    RecipeDao completed

--- src/main/java/com/example/backbyteacookie/DAO/RecipeDao.java
+++ src/main/java/com/example/backbyteacookie/DAO/RecipeDao.java
@@ -1,9 +1,15 @@
 package com.example.backbyteacookie.DAO;
 
+import com.example.backbyteacookie.models.Comment;
 import com.example.backbyteacookie.models.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RecipeDao extends JpaRepository<Recipe, Long> {
+    @Query("SELECT r FROM Recipe r WHERE r.author_id = :userId")
+    List<Recipe> findRecipesByUserId(Long userId);
 }
