c0a1948ade61604bccc33b89eb17d61b1f844899
Émile <emile.royer@epfedu.fr>
2023-10-10 14:07:47 +0200

    Add database schema for PostgreSQL

---
+++ initdb/1_TABLES.sql
@@ -0,0 +1,44 @@
+CREATE TABLE users (
+    id serial PRIMARY KEY,
+    name text NOT NULL,
+    email text NOT NULL,
+    password text NOT NULL
+);
+
+CREATE TYPE difficulty AS ENUM ( 'easy', 'medium', 'hard' );
+
+CREATE TABLE recipes (
+    id serial PRIMARY KEY,
+    name text NOT NULL,
+    image bytea,
+    description text NOT NULL,
+    difficulty difficulty NOT NULL,
+    time interval HOUR TO MINUTE NOT NULL,
+    author_id integer REFERENCES users ON DELETE CASCADE,
+    date timestamp with time zone NOT NULL
+);
+
+CREATE TYPE ingredient_type AS ENUM ( 'discreet', 'mass', 'volume' );
+
+CREATE TABLE ingredients (
+    id serial PRIMARY KEY,
+    name text NOT NULL,
+    image bytea,
+    type ingredient_type NOT NULL
+);
+
+CREATE TABLE ingredient_recipe (
+    id serial PRIMARY KEY,
+    recipe_id integer REFERENCES recipes ON DELETE CASCADE,
+    ingredient_id integer REFERENCES ingredients ON DELETE RESTRICT ,
+    quantity real NOT NULL
+);
+
+CREATE TABLE comments (
+    id serial PRIMARY KEY,
+    author_id integer REFERENCES users ON DELETE CASCADE,
+    recipe_id integer REFERENCES recipes ON DELETE CASCADE,
+    rating integer CHECK (rating >= 0 and rating <= 10) NOT NULL,
+    body text NULL,
+    date timestamp with time zone NOT NULL
+);
