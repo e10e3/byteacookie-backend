7c0a2eb105d6060c52907776fd94a5b117a2033c
Lucie LONG <107046732+lucielong@users.noreply.github.com>
2023-10-10 10:26:54 +0200

    Initial commit

---
+++ src/main/java/com/example/backbyteacookie/BackByteacookieApplication.java
@@ -0,0 +1,13 @@
+package com.example.backbyteacookie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackByteacookieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackByteacookieApplication.class, args);
+	}
+
+}
