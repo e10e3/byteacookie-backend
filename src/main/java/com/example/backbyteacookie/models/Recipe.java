f096919755ce28f2cb8e3b09aaf22b3ab81fa577
Émile <emile.royer@epfedu.fr>
2023-10-16 10:39:11 +0200

    Map Java Duration to Postgre interval
    Hibernate by default maps Duration to integer.
    Fixes querying for recipes and comments

--- src/main/java/com/example/backbyteacookie/models/Recipe.java
+++ src/main/java/com/example/backbyteacookie/models/Recipe.java
@@ -1,13 +1,17 @@
 package com.example.backbyteacookie.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import org.hibernate.annotations.JdbcTypeCode;
 
 import java.time.Duration;
 import java.time.ZonedDateTime;
 import java.util.List;
+
+import static org.hibernate.type.SqlTypes.INTERVAL_SECOND;
 
 @Entity
 @Table(name = "recipes")
@@ -23,6 +27,7 @@ public class Recipe {
     private String description;
     @Enumerated(EnumType.STRING)
     private Difficulty difficulty;
+    @JdbcTypeCode(INTERVAL_SECOND)
     private Duration time;
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "author_id")
@@ -35,6 +40,7 @@ public class Recipe {
             inverseJoinColumns = @JoinColumn(name = "recipe_id"))
     private List<Ingredient> ingredients;
     @OneToMany(mappedBy = "recipe")
+    @JsonIgnore
     private List<Comment> comments;
 
     public Recipe() {
